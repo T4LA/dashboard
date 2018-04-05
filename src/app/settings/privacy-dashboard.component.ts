709a6d805d788baa9b07e6c75459d6ae42b0e813
Daniel Biedermann <biedermann@dipf.de>
2018-04-05 15:03:35 +0200

    grid layout for data settings

--- src/app/settings/privacy-dashboard.component.ts
+++ src/app/settings/privacy-dashboard.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { MockCourseService } from '../services/mock/course.service';
 
 @Component({
     selector: 'privacy-dashboard',
-    template: require('./privacy-dashboard.component.html')
+    template: require('./privacy-dashboard.component.html'),
+    styleUrls: ['./privacy-dashboard.component.css']
 })
 
 export class PrivacyDashboardComponent {
-    
+    courseData: any;
+
+    constructor(private courseService: MockCourseService) {
+        this.courseData = courseService.getCourseSettings();
+    }
 }
