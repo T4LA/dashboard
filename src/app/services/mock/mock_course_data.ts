ae338bc54dfb4ebb7625acb9d44f715a6a15b6aa
Daniel Biedermann <biedermann@dipf.de>
2018-04-10 10:05:54 +0200

    fixed data subscription to mock data

--- src/app/services/mock/mock_course_data.ts
+++ src/app/services/mock/mock_course_data.ts
@@ -6,31 +6,94 @@ export const data = [
         'widgets': [
             {
                 "name": "Assignment Grade Chart",
-                "data":[
+                "data": [
                     "Assignment Grades"
                 ]
             },
             {
                 "name": "Goal Attainment Widget",
-                "data":[
+                "data": [
                     "Number of videos watched"
                 ]
             }
         ],
-        "data":[
+        "data": [
             {
                 'name': 'Number of Forum Posts',
+                'description': "The number of posts that you have written in the course forum",
                 'enabled': true,
                 'canBeDisabled': true
             },
             {
                 'name': 'Assignment Grades',
+                'description': "The grades that you have received for your assignments in the course",
                 'enabled': true,
                 'canBeDisabled': false
             },
             {
                 'name': 'Number of videos watched',
+                'description': "The number of course relevant videos that you have watched",
                 'enabled': true,
+                'canBeDisabled': true
+            }
+            ,
+            {
+                'name': 'Number of messages to peers',
+                'description': "The number of messages that you sent to other course participants",
+                'enabled': false,
+                'canBeDisabled': true
+            }
+        ]
+    },
+    {
+        "title": "Computer Graphics Lecture",
+        "lecturer": "Detlef Krömker",
+        "startDate": "07.04.2018",
+        'widgets': [
+            {
+                "name": "Assignment Grade Chart",
+                "data": [
+                    "Assignment Grades"
+                ]
+            },
+            {
+                "name": "Goal Attainment Widget",
+                "data": [
+                    "Number of videos watched"
+                ]
+            },
+            {
+                "name": "Compiler Warning Rank",
+                "data": [
+                    "Number of compiler warnings"
+                ],
+                "description": "A widget that displays the top 10 of the students with least number of compiler warnings for the assignments"
+            }
+        ],
+        "data": [
+            {
+                'name': 'Number of compiler warnings',
+                'description': "The number compiler warnings that still remained when you submitted the assignments.",
+                'enabled': true,
+                'canBeDisabled': true
+            },
+            {
+                'name': 'Assignment Grades',
+                'description': "The grades that you have received for your assignments in the course",
+                'enabled': true,
+                'canBeDisabled': false
+            },
+            {
+                'name': 'Number of videos watched',
+                'description': "The number of course relevant videos that you have watched",
+                'enabled': true,
+                'canBeDisabled': true
+            }
+            ,
+            {
+                'name': 'Date of Assignment Submission',
+                'description': "The number of messages that you sent to other course participants",
+                'enabled': false,
                 'canBeDisabled': true
             }
         ]
--- src/app/settings/privacy-dashboard.component.ts
+++ src/app/settings/privacy-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MockCourseService } from '../services/mock/course.service';
 
 @Component({
@@ -9,9 +9,29 @@ import { MockCourseService } from '../services/mock/course.service';
 
 export class PrivacyDashboardComponent {
     courseData: any;
-    selectedData: any;
+    selectedDataItem: any;
+    selectedRow: Number;
+    selectedCourse: Number;
 
     constructor(private courseService: MockCourseService) {
-        this.courseData = courseService.getCourseSettings();
+        //this.courseData = courseService.getCourseSettings();
+    }
+
+    ngOnInit() {
+        this.courseService.getCourseSettings().subscribe(data => {
+            console.log("course data", data);
+            this.courseData = data;
+        });
+    }
+
+    highlightClickedRow = function (courseIndex: any, rowIndex: any) {
+        this.selectedRow = rowIndex;
+        this.selectedCourse = courseIndex;
+
+        console.log("course data: ", this.courseData);
+
+        this.selectedDataItem = this.courseData[this.selectedCourse]["data"][this.selectedRow];
+
+        console.log("Clicked row: ", this.selectedDataItem);
     }
 }
