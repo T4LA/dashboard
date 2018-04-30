e845e9bd929734c633d496280402e6559028bcf7
Daniel Biedermann <biedermann@dipf.de>
2018-04-30 12:58:23 +0200

    selected data item shows related widgets

--- src/app/settings/privacy-dashboard.component.ts
+++ src/app/settings/privacy-dashboard.component.ts
@@ -1,37 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { MockCourseService } from '../services/mock/course.service';
+import { Component, OnInit } from "@angular/core";
+import { MockCourseService } from "../services/mock/course.service";
 
 @Component({
-    selector: 'privacy-dashboard',
-    template: require('./privacy-dashboard.component.html'),
-    styleUrls: ['./privacy-dashboard.component.css']
+  selector: "privacy-dashboard",
+  template: require("./privacy-dashboard.component.html"),
+  styleUrls: ["./privacy-dashboard.component.css"]
 })
-
 export class PrivacyDashboardComponent {
-    courseData: any;
-    selectedDataItem: any;
-    selectedRow: Number;
-    selectedCourse: Number;
+  courseData: any;
+  selectedDataItem: any;
+  selectedRow: number;
+  selectedCourseIndex: number;
 
-    constructor(private courseService: MockCourseService) {
-        //this.courseData = courseService.getCourseSettings();
+  constructor(private courseService: MockCourseService) {
+    //this.courseData = courseService.getCourseSettings();
+  }
+
+  ngOnInit() {
+    this.courseService.getCourseSettings().subscribe(data => {
+      console.log("course data", data);
+      this.courseData = data;
+    });
+  }
+
+  getWidgetsForSelected() {
+    let widgetsForData = [];
+    for (let w of this.courseData[this.selectedCourseIndex].widgets) {
+      for (let d of w.data) {
+        if (d === this.selectedDataItem.name) {
+          if (widgetsForData.findIndex(x => x.name === w.name) === -1) {
+            widgetsForData.push(w);
+          }
+        }
+      }
     }
+    console.log("Widgets for data: ", widgetsForData);
+    return widgetsForData;
+  }
 
-    ngOnInit() {
-        this.courseService.getCourseSettings().subscribe(data => {
-            console.log("course data", data);
-            this.courseData = data;
-        });
-    }
+  highlightClickedRow = function(courseIndex: any, rowIndex: any) {
+    this.selectedRow = rowIndex;
+    this.selectedCourseIndex = courseIndex;
 
-    highlightClickedRow = function (courseIndex: any, rowIndex: any) {
-        this.selectedRow = rowIndex;
-        this.selectedCourse = courseIndex;
+    console.log("course data: ", this.courseData);
 
-        console.log("course data: ", this.courseData);
+    this.selectedDataItem = this.courseData[this.selectedCourseIndex]["data"][
+      this.selectedRow
+    ];
 
-        this.selectedDataItem = this.courseData[this.selectedCourse]["data"][this.selectedRow];
-
-        console.log("Clicked row: ", this.selectedDataItem);
-    }
+    console.log("Clicked row: ", this.selectedDataItem);
+  };
 }
